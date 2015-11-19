8e649a5ec71001362728b2becad9061be33579ee
Povilas Kanapickas <povilas@unity3d.com>
2015-11-19 12:18:10 +0200

    Temporarily work around a il2cpp bug on 5.3

--- demo/Assets/AssetBundleManager/AssetBundleManager.cs
+++ demo/Assets/AssetBundleManager/AssetBundleManager.cs
@@ -237,8 +237,14 @@ namespace AssetBundles
             return operation;
         }
 
+        // Temporarily work around a il2cpp bug
+        static protected void LoadAssetBundle(string assetBundleName)
+        {
+            LoadAssetBundle(assetBundleName, false);
+        }
+            
         // Load AssetBundle and its dependencies.
-        static protected void LoadAssetBundle(string assetBundleName, bool isLoadingAssetBundleManifest = false)
+        static protected void LoadAssetBundle(string assetBundleName, bool isLoadingAssetBundleManifest)
         {
             Log(LogType.Info, "Loading Asset Bundle " + (isLoadingAssetBundleManifest ? "Manifest: " : ": ") + assetBundleName);
 
